9124a4070721018d6c5da67e8b2b3f38aaf15fe5
Abhya kumar sen <your-email@example.com>
2026-07-14 11:20:25 +0530

    done

--- src/main/java/GitClass.java
+++ src/main/java/GitClass.java
@@ -4,7 +4,7 @@ public class GitClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println("This code is inside the GitHubRepository");
+		System.out.println("This code is inside the GitHubRepo");
 
 	}
 
